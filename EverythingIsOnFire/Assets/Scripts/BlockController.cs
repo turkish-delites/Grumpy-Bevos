69c75250a40f8f8c2eb0b514f298a690ffcbee79
Austin Miller <Austin@Austins-MacBook-Pro-2.local>
2018-08-31 14:53:10 -0500

    Started working on configuration

--- EverythingIsOnFire/Assets/Scripts/BlockController.cs
+++ EverythingIsOnFire/Assets/Scripts/BlockController.cs
@@ -5,14 +5,20 @@ using System.Linq;
 
 public class BlockController : MonoBehaviour, IFireGroupController
 {
+
+    public List<FirePointController> _firePoints;
     [SerializeField]
-    private List<FirePointController> _firePoints;
+    private GameObject firePointPrefab;
     [SerializeField]
     private GameObject _mainBlockObject;
     [SerializeField]
     private float _piercingShotSpeedToBreak = 0f;
     [SerializeField]
     private float _secondsFromFullBurnToBreak = 1f;
+
+    void Start(){
+        AddFirePoints();
+    }
 
     // Update is called once per frame
     void Update () {
@@ -52,5 +58,19 @@ public class BlockController : MonoBehaviour, IFireGroupController
             collision.GetComponent<Rigidbody2D>().velocity = collision.GetComponent<Rigidbody2D>().velocity * .5f;
             Destroy(_mainBlockObject);
         }
+    }
+
+    private void AddFirePoints(){
+        var size = _mainBlockObject.transform.localScale;
+        Debug.Log(size);
+        AddFirePoint(0, 0);
+    }
+
+    private void AddFirePoint(float x, float y){
+        GameObject obj = Instantiate(firePointPrefab) as GameObject;
+        obj.GetComponent<FirePointController>().FirePointGroupController = this.gameObject;
+        obj.transform.parent = _mainBlockObject.transform;
+        obj.transform.position = new Vector3(x, y, 0);
+        _firePoints.Add(obj.GetComponent<FirePointController>());
     }
 }
