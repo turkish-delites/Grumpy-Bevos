faf5ba5a64e3d4b1e30c7686ccd7bfeb7725f757
ryan-chomistek <ryan.chomistek@tamu.edu>
2018-09-01 15:40:48 -0500

    lots of fixes

---
+++ EverythingIsOnFire/Assets/Scripts/RestartButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class RestartButton : MonoBehaviour {
+    public void RestartGame()
+    {
+        LevelManager.Instance.RestartGame();
+    }
+
+    public void RestartLevel()
+    {
+        LevelManager.Instance.ReloadScene();
+    }
+
+    public void SkipLevel()
+    {
+        LevelManager.Instance.NextScene();
+    }
+}
